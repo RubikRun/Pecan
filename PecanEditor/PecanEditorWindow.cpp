18f6627219028e7d0c9d5b51ad06c064670ffa93
Boris Dimitrov <borisd2000@gmail.com>
2024-05-30 19:23:23 +0200

    draw simple triangle with OpenGL

--- PecanEditor/PecanEditorWindow.cpp
+++ PecanEditor/PecanEditorWindow.cpp
@@ -3,27 +3,61 @@
 
 #include <QOpenGLContext>
 
-PecanEditorWindow::PecanEditorWindow(QWidget *parent)
+PecanEditorWindow::PecanEditorWindow(QWidget* parent)
+	: startTime(std::chrono::high_resolution_clock::now())
 {
+	QSurfaceFormat format;
+	format.setVersion(4, 5);
+	format.setProfile(QSurfaceFormat::CoreProfile);
+	setFormat(format);
 }
 
 PecanEditorWindow::~PecanEditorWindow()
-{}
+{
+	glDeleteVertexArrays(1, &vertexArrayObjectID);
+}
 
 void PecanEditorWindow::initializeGL() {
-    initializeOpenGLFunctions();
-    // Log OpenGL info
-    PECAN_LOG_INFO("OpenGL Vendor: " << glGetString(GL_VENDOR));
-    PECAN_LOG_INFO("OpenGL Version: " << glGetString(GL_VERSION));
-    PECAN_LOG_INFO("OpenGL Renderer: " << glGetString(GL_RENDERER));
-    // Set clear color to black
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	initializeOpenGLFunctions();
+	// Log OpenGL info
+	PECAN_LOG_INFO("OpenGL Vendor: " << glGetString(GL_VENDOR));
+	PECAN_LOG_INFO("OpenGL Version: " << glGetString(GL_VERSION));
+	PECAN_LOG_INFO("OpenGL Renderer: " << glGetString(GL_RENDERER));
+
+	setupTriangle();
 }
 
 void PecanEditorWindow::resizeGL(int w, int h) {
-    glViewport(0, 0, w, h);
+	glViewport(0, 0, w, h);
+}
+
+void PecanEditorWindow::setupTriangle() {
+	// Create and bind vertex array
+	glCreateVertexArrays(1, &vertexArrayObjectID);
+	glBindVertexArray(vertexArrayObjectID);
+	// Create and bind vertex buffer
+	glGenBuffers(1, &vertexBufferObjectID);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjectID);
+	// Data of triangle's vertices
+	const GLfloat vertices[3 * 3] = {
+		-0.5f, -0.5f, 0.0f,
+		+0.5f, -0.5f, 0.0f,
+		+0.0f, +0.5f, 0.0f
+	};
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// Declare position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), reinterpret_cast<GLvoid*>(0));
+	// Enable position attribute
+	glEnableVertexAttribArray(0);
+	// Unbind vertex array
+	glBindVertexArray(0);
 }
 
 void PecanEditorWindow::paintGL() {
-    glClear(GL_COLOR_BUFFER_BIT);
+	// Draw triangle
+	glBindVertexArray(vertexArrayObjectID);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glBindVertexArray(0);
+	// Manually call update() so that paintGL() is immediately called again
+	update();
 }
--- PecanEditor/PecanEditorWindow.h
+++ PecanEditor/PecanEditorWindow.h
@@ -1,18 +1,26 @@
 #pragma once
 
 #include <QtOpenGL/QOpenGLWindow>
-#include <QOpenGLFunctions>
+#include <QOpenGLFunctions_4_5_Core>
 
-class PecanEditorWindow : public QOpenGLWindow, private QOpenGLFunctions
+#include <chrono>
+
+class PecanEditorWindow : public QOpenGLWindow, private QOpenGLFunctions_4_5_Core
 {
     Q_OBJECT
 
 public:
-    PecanEditorWindow(QWidget *parent = nullptr);
+    PecanEditorWindow(QWidget* parent = nullptr);
     ~PecanEditorWindow();
 
-private:
+private: /* functions */
     void initializeGL() override;
     void resizeGL(int w, int h) override;
     void paintGL() override;
+
+    void setupTriangle();
+
+private: /* variables */
+    GLuint vertexArrayObjectID = 0;
+    GLuint vertexBufferObjectID = 0;
 };
